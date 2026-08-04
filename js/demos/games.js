d7b32a059d21466d91f1b0fb660125b56ee2ec73
Bradley Matera <162147754+BradleyMatera@users.noreply.github.com>
2026-08-04 10:32:42 -0500

    release: add game module exports

---
+++ js/demos/games.js
@@ -0,0 +1,6 @@
+export { createCloudOpsGame } from './games/cloud-ops.js';
+export { createGolfGame } from './games/golf.js';
+export { createCodeDungeon } from './games/code-dungeon.js';
+export { createDataCenterDefense } from './games/data-center-defense.js';
+export { createEvolutionGame } from './games/evolution.js';
+export { createPcsLogistics } from './games/pcs-logistics.js';
