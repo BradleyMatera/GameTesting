616a1f508e06153b2feebf53d69f3d51cbc08199
Bradley Matera <162147754+BradleyMatera@users.noreply.github.com>
2026-08-04 10:45:21 -0500

    release: add website module exports

---
+++ js/demos/websites.js
@@ -0,0 +1,5 @@
+export { createMateraDigital } from './websites/matera-digital.js';
+export { createFairwayStore } from './websites/fairway-store.js';
+export { createEthicsLms } from './websites/ethics-lms.js';
+export { createConstructionErp } from './websites/construction-erp.js';
+export { createRecruiterPortfolio } from './websites/recruiter-portfolio.js';
