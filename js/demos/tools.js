9785e4cfa62bb87ac8e37bab6cf8411b2d68742a
Bradley Matera <162147754+BradleyMatera@users.noreply.github.com>
2026-08-04 10:33:01 -0500

    release: add tool module exports

---
+++ js/demos/tools.js
@@ -0,0 +1,7 @@
+export { createArchitectureBuilder } from './tools/architecture-builder.js';
+export { createWorkflowDesigner } from './tools/workflow-designer.js';
+export { createSeoAuditor } from './tools/seo-auditor.js';
+export { createAccessibilityLab } from './tools/accessibility-lab.js';
+export { createApiFailureLab } from './tools/api-failure-lab.js';
+export { createShaderLab } from './tools/shader-lab.js';
+export { createWorldGenerator } from './tools/world-generator.js';
